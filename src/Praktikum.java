fd6da9b32d602521be4380f37a46926d491c790e
ka5trulkin <ka5trulkin@yandex.by>
2022-09-17 18:05:56 +0300

    create programmers-day

---
+++ src/Praktikum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("День программиста 12.09." + year + ".");
+        } else {
+            System.out.println("День программиста 13.09." + year + ".");
+        }
+    }
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0;
+    }
+}
